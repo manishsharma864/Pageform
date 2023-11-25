1a1cebaeade5921916b454e61d3b96acdd3fc659
Manish Sharma <81001090+manishsharma864@users.noreply.github.com>
2023-11-25 10:42:57 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PageForm ",
+  title: "CustomForms ",
   description: "One Step Solution for Custom Forms",
 };
 
